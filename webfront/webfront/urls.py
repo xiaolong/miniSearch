cec4ad3a6378558aed305a59521af16d104c2b3a
Xiaolong Cheng <xiaolong.ch@gmail.com>
2012-04-21 22:26:09 -0700

    web tested, admin page left unfixed, need better DB designs.

--- webfront/webfront/urls.py
+++ webfront/webfront/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from views import home_view
+import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    ('^$',home_view),
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^$',views.home_view),
+    (r'^search/$',views.search),
 )
--- webfront/webfront/views.py
+++ webfront/webfront/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render_to_response
 import MySQLdb
 
+from django.http import HttpResponse
 
 def home_view(request):
 	return render_to_response('index.html')
+	
+def search(request):
+	#return render_to_response('results.html',{'word': request.GET['userInput']})
+	db=MySQLdb.connect(user='root',db='test', passwd='cxl', host='localhost')
+	cursor=db.cursor()
+	cursor.execute('select name from girls')
+	names=[row[0] for row in cursor.fetchall()]
+	db.close()
+	return render_to_response('results.html',{'Qresults':names})
 
